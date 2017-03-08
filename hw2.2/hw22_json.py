c343c1a4ba5049220e2f9192bb777c4a5425faf7
majorant <your_email@whatever.com>
2017-03-08 17:37:40 +0300

    add work with xml

--- hw2.2/hw22_json.py
+++ hw2.2/hw22_json.py
@@ -1,65 +1,99 @@
 '''
-# ДЗ 2.3
+# ДЗ 2.2
 # программа по заданным четырём файлам в формате json (xml)
 # выводит топ10 чаще всего встречающихся слов
 '''
 
-'''
-file -bi newsit.json
-text/plain; charset=unknown-8bit
-file -bi newsfr.json
-text/html; charset=iso-8859-1
-file -bi newscy.json
-text/html; charset=iso-8859-1
-file -bi newsafr.json
-text/html; charset=utf-8
-
-import urllib.parse
-
-url = "https://ru.wikipedia.org/wiki/%D0%9C%D0%B5%D1%82%D0%BE%D0%B4_%D0%BC%D0%BD%D0%BE%D0%B6%D0%B8%D1%82%D0%B5%D0%BB%D0%B5%D0%B9_%D0%9B%D0%B0%D0%B3%D1%80%D0%B0%D0%BD%D0%B6%D0%B0"
-print(urllib.parse.unquote(url))
-'''
 # import urllib.parse
 import codecs
 import json
 import re
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import ParseError
 
 
-# пытаемся понять кодировку файла
-def check_coding(file_name):
+def isxml(file_name):
+    '''прроверяет является ли файл xml по расширению )'''
+    # если подать json формат в файле bb.xml, то упадёт
+    if file_name.endswith('.xml'):
+        return True
+    else:
+        return False
+
+
+def check_encoding(file_name):
     '''возвращает кодировку в файле
     для этого открывает его, проверяет есть ли слово 'новости'' в заголовке
     '''
+    # по внутренним ощещениями это жуткий костыль, но ничего интереснее не придумал (
+    # решение из группы:
+    # import chardet
+    # def check_encoding(news_file):
+    # rawdata = open(news_file, "rb").read()
+    # result = chardet.detect(rawdata)
+    # open(news_file).close()
+    # return result['encoding']
     codings = [
-        'utf8',
+        'utf-8',
         'koi8-r',
         'iso8859_5',
         'cp1251'
         ]
     for code in codings:
-        try:
-            with codecs.open(file_name, encoding=code) as news:
-                js = json.load(news)
-                if 'новости' in js['rss']['channel']['title'].lower().split():
+        # if *.xml
+        if isxml(file_name):
+            try:
+                parser = ET.XMLParser(encoding=code)
+                tree = ET.parse(file_name, parser=parser)
+                root = tree.getroot()
+                # если есть ожидаемые слова
+                if 'новости' in root[0][0].text.lower().split():
                     return code
                 else:
                     continue
-        # что произойдёт в windows?
-        except UnicodeDecodeError:
-            continue
+            # при попытке парсинга могут попасться символы, которые not allowed:
+            # set(['&#x08;', '&#x0E;', '&#x1E;', '&#x1C;', '&#x18;', '&#x04;',
+            #'&#x0A;', '&#x0C;', '&#x16;', '&#x14;', '&#x06;', '&#x00;', '&#x10;
+            #', '&#x02;', '&#x0D;', '&#x1D;', '&#x0F;', '&#x09;', '&#x1B;',
+            #'&#x05;', '&#x15;', '&#x01;', '&#x03;'])
+            except ParseError:
+                continue
+        else:
+            # if *.json
+            try:
+                with codecs.open(file_name, encoding=code) as news:
+                    text = json.load(news)
+                    if 'новости' in text['rss']['channel']['title'].lower().split():
+                        return code
+                    else:
+                        continue
+            # что произойдёт в windows?
+            except UnicodeDecodeError:
+                continue
     print('не получилось определить кодировку для ', file_name)
     return None
 
 
-def json_text_to_list(file_name):
-    '''на вход получает имя файла,
+def xml_text_to_list(file_name, encode):
+    '''для файлов формата xml возвращает список слов в статьях
+    на вход получает имя файла *.xml, кодировку
     возвращает список слов со всех статей в lowercase'''
     words_lst = []
-    # определяем кодировку файла
-    encode = check_coding(file_name)
-    # если не смогли определить кодировку
-    if encode is None:
-        return None
+    parser = ET.XMLParser(encoding=encode)
+    tree = ET.parse(file_name, parser=parser)
+    root = tree.getroot()
+    # ходим по корню, ищем description - это статьи
+    for new in root.iter('description'):
+        temp_lst = remove_waste_chars(new.text)
+        words_lst += temp_lst
+    return words_lst
+
+
+def json_text_to_list(file_name, encode):
+    '''для файлов формата json возвращает список слов в статьях
+    на вход получает имя файла *.json, кодировку
+    возвращает список слов со всех статей в lowercase'''
+    words_lst = []
     with codecs.open(file_name, encoding=encode) as news:
         js = json.load(news)
         for article in js['rss']['channel']['item']:
@@ -76,8 +110,24 @@ def json_text_to_list(file_name):
     return words_lst
 
 
+def text_to_list(file_name):
+    '''на вход получает имя файла,
+    возвращает список слов со всех статей в lowercase'''
+    words_lst = []
+    # определяем кодировку файла
+    encode = check_encoding(file_name)
+    # если не смогли определить кодировку
+    if encode is None:
+        return None
+    # выбираем чем пользоваться в зависимости от файла
+    if isxml(file_name):
+        return xml_text_to_list(file_name, encode)
+    else:
+        return json_text_to_list(file_name, encode)
+
+
 def remove_waste_chars(string):
-    '''несмотря на название, выбирает слова по регулярному выражению:
+    '''выбирает слова по регулярному выражению:
     r'[а-яА-Я]{6,}'''
     # регулярное выражение: слова на русском, более 6 символов
     reg = r'[а-яА-Я]{6,}'
@@ -104,13 +154,12 @@ def top10(words_lst):
     return top10_words
 
 def print_top10(words_lst):
-    '''печатает топ10 слов'''
+    '''печатает слова из списка в строку'''
     for i in range(0, len(words_lst)):
         if i != len(words_lst)-1:
             print(words_lst[i], end=', ')
         else:
             print(words_lst[i], end='.\n')
-
 
 
 def file_handle():
@@ -121,12 +170,16 @@ def file_handle():
         'newsfr.json',
         'newsafr.json',
         'newscy.json',
-        'newsit.json'
+        'newsit.json',
+        'newsfr.xml',
+        'newsafr.xml',
+        'newscy.xml',
+        'newsit.xml'
     ]
     for js_file in file_list_json:
         print('для файла \"{}\" top-10 слов:'.format(js_file))
         #top10_words = json_handle(js_file)
-        words_lst = json_text_to_list(js_file)
+        words_lst = text_to_list(js_file)
         if words_lst is not None:
             top10_words = top10(words_lst)
             print_top10(top10_words)
