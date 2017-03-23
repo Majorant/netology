5df692902114b432c6cc92bf2d1c4b9934a349dc
majorant <majorant@yandex.ru>
2017-03-23 18:09:18 +0300

    Revert "some features, NOT WORKED YET"
    This reverts commit ff79ca8fd12f93f216c2e25fc2f6eea307c3d4a7.
    
    лишнее телодвижение

--- hw26/hw26.py
+++ hw26/hw26.py
@@ -13,57 +13,26 @@ def resize_photo(photos, path, size = 200):
     return
 
 
-def input_handle(input_string):
-    param_lst = input_string.split()
-    default_params = ['Source', '*.jpg']
-    if not param_lst:
-        return default_params[0], default_params[1]
-    elif len(param_lst) == 1:
-        return param_lst[0], default_params[1]
-    elif len(param_lst) == 2:
-        return param_lst[0], param_lst[1]
-    else:
-        return param_lst[0], param_lst[1:]
-
-def create_photos_lst(path, extensions):
-    photos = []
-    print(path)
-    print(extensions)
-    if os.path.exists(path):
-        for extension in extensions:
-            photos_path = os.path.join(path, extension)
-            photos.append(glob.glob(path))
-    else:
-        print('заданный путь не существут')
-        exit(1)
-
-
 def main_func():
     # define, where photos
-    print('введите путь до папки с фотографиями и расширения, с которыми предполагается работать, чрез пробел:')
-    inp_lst = input('input path to photos and extensions (default: "Source jpg": ')
-    input_path, extension = input_handle(inp_lst)
-    path_to_photo = os.path.join(os.path.curdir, input_path)
+    #print('введите путь до папки с фотографиями и расширения, с которыми предполагается работать чрез пробел:')
+    #inp_lst = input('input path to photos and extensions (default: Source and *.jpg)')
 
-    # меняем рабочую папку на папку с файлом
     realpath = os.path.dirname(os.path.realpath(__file__))
+    # меняем рабочую папку на папку с файлом
     os.chdir(realpath)
-    # каталог для результатов
-    result_path = os.path.join(realpath, 'Result')
-
-    photos_lst = create_photos_lst(path_to_photo, extension)
-    print(input_path)
-    print(extension)
-
-    print(photos_lst)
+    # input_path = 'Source'
+    # extension = '*.jpg'
+    path_to_photo = os.path.join('Source', '*.jpg')
+    photos_lst = glob.glob(path_to_photo)
     # создаём папку Result/, если её нет
+    result_path = os.path.join('Result')
     # try:
     #     os.mkdir(result_path)
     # except FileExistsError:
     #     pass
     if not os.path.exists(result_path):
         os.mkdir(result_path)
-
 
     resize_photo(photos_lst, result_path)
 
