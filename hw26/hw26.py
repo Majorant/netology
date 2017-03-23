ffddf57c967d5274d231f0c16199f9cda813592a
Majorant <majorant@yandex.ru>
2017-03-23 21:53:40 +0300

    minor chanhes

--- hw26/hw26.py
+++ hw26/hw26.py
@@ -7,24 +7,29 @@ import subprocess
 
 
 def resize_photo(photos, path, size = 200):
+    '''функция вызывает внешнюю программу imsgemagick и передаёт параметры
+    subprocess.run() работает только в версиях 3,5 и выше'''
     for photo in photos:
-        # print(photo)
+        # os.path.basename(photo) возвращает имя каждго файла
         output = os.path.join(path, os.path.basename(photo))
         subprocess.run(['convert', photo, '-resize', str(size), output])
-    return
 
 
 def modify_extensions(extensions):
-    exten = extensions.copy()
-    for i in range(len(exten)):
-        if not exten[i].startswith('*.'):
-            exten[i] = '*.' + exten[i]
+    '''возвращает список расширений вида "*.smth"
+    '''
+    # exten = extensions.copy()
+    # вот тут вопрос
+    for i in range(len(extensions)):
+        if not extensions[i].startswith('*.'):
+            extensions[i] = '*.' + extensions[i]
         else:
             continue
-    return exten
+    return extensions
 
 
 def input_handle(extensions):
+    '''функция возвращает список с расширениями, по которым будем искать файлы'''
     default_extension = ['*.jpg']
     if not extensions:
         return default_extension
@@ -33,6 +38,7 @@ def input_handle(extensions):
 
 
 def create_photos_lst(path, extensions):
+    '''функция возвращает список всех файлов, размер которых надо изменить'''
     photos = []
     for extension in extensions:
         photos_path = os.path.join(path, extension)
@@ -41,11 +47,10 @@ def create_photos_lst(path, extensions):
 
 
 def main_func():
+    '''основная функция'''
     # define, where photos
-    print('введите расширения, с которыми предполагается работать, через пробел:')
-    inp_lst = input('input extensions (by default jpg): ').split()
+    inp_lst = input('введите расширения файлов с картинками через пробел: (by default jpg): ').split()
     extension = input_handle(inp_lst)
-
     # меняем рабочую папку на папку с файлом
     realpath = os.path.dirname(os.path.realpath(__file__))
     os.chdir(realpath)
@@ -53,10 +58,13 @@ def main_func():
     path_to_photo = os.path.join(realpath, 'Source')
     # каталог для результатов
     result_path = os.path.join(realpath, 'Result')
+    # получаем список всех изображений
     photos_lst = create_photos_lst(path_to_photo, extension)
     # создаём папку Result/, если её нет
     if not os.path.exists(result_path):
         os.mkdir(result_path)
-
+    # меняем размер изображений, складываем в result_path
     resize_photo(photos_lst, result_path)
+
+    
 main_func()
